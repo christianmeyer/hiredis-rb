019cf9dae74e8e1e2198d4a9d758320b63fd38ab
nerd <github@rchristian.de>
2012-04-22 21:53:41 +0200

    let it compile under BSD

--- ext/hiredis_ext/extconf.rb
+++ ext/hiredis_ext/extconf.rb
@@ -8,10 +8,14 @@ unless File.directory?(hiredis_dir)
   exit 1
 end
 
+BSD = Config::CONFIG['host_os'].downcase =~ /bsd/
+GMAKE_CMD = Config::CONFIG['host_os'].downcase =~ /bsd|solaris/ ? "gmake" : "make"
+
 # Make sure hiredis is built...
-system("cd #{hiredis_dir} && make static")
+system("cd #{hiredis_dir} && #{GMAKE_CMD} static")
 
 # Statically link to hiredis (mkmf can't do this for us)
 $CFLAGS << " -I#{hiredis_dir}"
+$CFLAGS << " -I/usr/local/include" if BSD
 $LDFLAGS << " #{hiredis_dir}/libhiredis.a"
 create_makefile('hiredis/ext/hiredis_ext')
